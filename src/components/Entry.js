52f3aad207c43a5eae8ad3b94ac94acf2ddb0e6a
Dan Ross <daniellross@gmail.com>
2021-04-21 09:50:38 -0500

    spent too much time writing map statement to get a comma seperated list

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -5,15 +5,14 @@ export const Entry = ({ entry }) => {
 
   const { deleteEntry, getEntryById, setEntry } = useContext(EntryContext)
 
-
-
   return (
 
     <section className="entry">
-      <div className="entry__concept">{entry.concept}</div>
-      <div className="entry__entry">{entry.entry}</div>
-      <div className="entry__date">{entry.date}</div>
-      <div className="entry__mood">{entry.mood.label}</div>
+      <div className="entry__concept"><b>Concept: </b> {entry.concept}</div>
+      <div className="entry__entry"><b>Entry: </b> {entry.entry}</div>
+      <div className="entry__date"><b>Date: </b> {entry.date}</div>
+      <div className="entry__mood"><b>Label: </b> {entry.mood.label}</div>
+      <div className="entry__tags"><b>Tags: </b> {entry.tags.map((tag, index) => index < (entry.tags.length -1)? tag.name + ', ' : tag.name)}</div>
 
       <button onClick={
         () => {
